df51d486caee2fa2112b82d75bdadd7810ec127e
Sergio Lima <sergiodelima22@gmail.com>
2022-12-14 15:31:53 -0300

    jogo-completado

--- Meus Jogos/main.py
+++ Meus Jogos/main.py
@@ -35,12 +35,23 @@ pos_alien_y = 360
 pos_player_x = 200
 pos_player_y = 300
 
-vel_x_missil = 0
-pos_x_missil = 250 #200 para que a imagem do missil fique sobreposta a da nave
-pos_y_missil = 300
+vel_missil_x = 0
+pos_missil_x = 250 #200 para que a imagem do missil fique sobreposta a da nave
+pos_missil_y = 300
+
+pontos = 3
+
+triggered = False
+
+
+font = pygame.font.SysFont('Fonts/PixelGameFont.ttf', 50)
 
 # 3 - Criando Loop para Manter a Janela Aberta:
 rodando = True
+# Para criar os objetos com as imagens:
+player_rect = playerImg.get_rect()
+alien_rect = alien.get_rect()
+missil_rect = missil.get_rect()
 
 # Funções:
 #Função para fazer o alien reaparecer em um determinado local depois de sair da tela.
@@ -50,7 +61,23 @@ def respawn():
     x = 1350
     y = random.randint(1,640)
     return [x,y]
+def respawn_missil():
+    triggered = False
+    respawn_missil_x = pos_player_x
+    respawn_missil_y = pos_player_y
+    vel_missil_x = 0
+    return [respawn_missil_x,respawn_missil_y,triggered,vel_missil_x]
 
+def colisions():
+    global pontos
+    if player_rect.colliderect(alien_rect) or alien_rect.x == 60:
+        pontos -= 1
+        return True
+    elif missil_rect.colliderect(alien_rect):
+        pontos += 1
+        return True
+    else:
+        return False
 
 # O fato do "rodando" já ser True dispensa a necessidade de colocar True depois de while no loop abaixo (eu acho).
 while rodando:
@@ -66,25 +93,67 @@ while rodando:
     if rel_x < 1280:
         screen.blit(bg, (rel_x, 0))
 
-    # Comando de movimento da nave:
+    # teclas:
     tecla = pygame.key.get_pressed()
     if tecla[pygame.K_UP] and pos_alien_y > 1:
         pos_player_y -= 1
+        if not triggered:
+            pos_missil_y -= 1
     if tecla[pygame. K_DOWN] and pos_player_y < 665:
         pos_player_y += 1
+        if not triggered:
+            pos_missil_y += 1
+
+    #Para disparar o missil da nave
+
+    if tecla[pygame.K_SPACE]:
+        triggered = True
+        vel_missil_x = 5
+
+    if pontos == -1:
+        rodando = False
 
     #respawn
-    if pos_alien_x == -5:
+    if pos_alien_x == 50:
         pos_alien_x = respawn()[0] # Zero corresponde a x na função respawn lá em cima
         pos_alien_y = respawn()[1] # Um corresponde a y na função respawn lá em cima
+    if pos_missil_x == 1300:
+        pos_missil_x, pos_missil_y, triggered,vel_missil_x = respawn_missil()
+
+    if pos_alien_x == 50 or colisions():
+        pos_alien_x = respawn()[0]
+        pos_alien_y = respawn()[1]
+
+    #posição rect
+
+    player_rect.y = pos_player_y
+    player_rect.x = pos_player_x
+
+    missil_rect.x = pos_missil_x
+    missil_rect.y = pos_missil_y
+
+    alien_rect.x = pos_alien_x
+    alien_rect.y = pos_alien_y
 
     #movimento
     x -= 2
     pos_alien_x -= 1
 
+    pos_missil_x += vel_missil_x
+
+    # Para saber se o programa está reconhecendo os objetos criados a partir das imagens.
+    pygame.draw.rect(screen, (255, 0, 0), player_rect, 4)
+    pygame.draw.rect(screen, (255, 0, 0), missil_rect, 4)
+    pygame.draw.rect(screen, (255, 0, 0), alien_rect, 4)
+
+    score = font.render(f'Pontos: {int(pontos)}', True, (0,0,0)) # Esses três zeros são a cor preta.
+    screen.blit(score, (50,50))
+
     #criando as imagens
     screen.blit(alien, (pos_alien_x, pos_alien_y))
-    screen.blit(missil, (pos_x_missil, pos_y_missil))
+    screen.blit(missil, (pos_missil_x,pos_missil_y))
     screen.blit(playerImg, (pos_player_x, pos_player_y))
+
+    print(pontos)
 
     pygame.display.update()
